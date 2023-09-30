24200b4959c58f4d4ee25ced3ca1c9efa4946620
Pedro Monteiro <pedromont436@gmail.com>
2023-09-29 23:09:04 -0300

    commit inicial

---
+++ backend/src/main/java/com/mont/controlevagas/ControleVagasApplication.java
@@ -0,0 +1,13 @@
+package com.mont.controlevagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleVagasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mont/controlevagas/ControleVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mont.controlevagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
