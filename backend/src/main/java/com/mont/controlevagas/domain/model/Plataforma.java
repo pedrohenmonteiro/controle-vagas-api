a95780cdfbd3138ec231a32b1ad4d7f3d30d79f6
Pedro Monteiro <pedromont436@gmail.com>
2023-10-02 19:34:02 -0300

    criando classe Plataforma

--- backend/src/main/java/com/mont/controlevagas/domain/model/Plataforma.java
+++ backend/src/main/java/com/mont/controlevagas/domain/model/Plataforma.java
@@ -1,32 +1,26 @@
 package com.mont.controlevagas.domain.model;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Candidatura {
-    
+@Entity
+public class Plataforma {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
-    
-    private String empresa;
-    private String descricao;
-    private CandidaturaStatus status;
-    private BigDecimal salario;
-    
-    @ManyToOne
-    @JoinColumn(name = "plataforma_id", nullable = false)
-    private Plataforma plataforma;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "plataforma")
+    private List<Candidatura> candidaturas;
 }
