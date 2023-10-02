604afd4be3ee263778e5c08e2a874d698fdb6024
Pedro Monteiro <pedromont436@gmail.com>
2023-10-02 20:20:41 -0300

    implementando classe UsuarioMapper

--- backend/src/main/java/com/mont/controlevagas/api/mapper/UsuarioMapper.java
+++ backend/src/main/java/com/mont/controlevagas/api/mapper/UsuarioMapper.java
@@ -1,12 +1,30 @@
 package com.mont.controlevagas.api.mapper;
 
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.mont.controlevagas.api.dto.UsuarioDto;
+import com.mont.controlevagas.domain.model.Usuario;
+
 
 @Component
 public class UsuarioMapper {
     
     @Autowired
     private ModelMapper modelMapper;
+
+    public Usuario toEntity(UsuarioDto usuarioDto) {
+        return modelMapper.map(usuarioDto, Usuario.class);
+    }
+
+    public UsuarioDto toDto(Usuario usuario) {
+        return modelMapper.map(usuario, UsuarioDto.class);
+    }
+
+    public List<UsuarioDto> toCollectionDto(List<Usuario> usuarios) {
+        return usuarios.stream().map(this::toDto).toList();
+    }
 }
