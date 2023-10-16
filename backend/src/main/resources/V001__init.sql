0d454b9f2ed48692fd792ad359062451a4699cb0
Pedro Monteiro <pedromont436@gmail.com>
2023-10-16 19:43:58 -0300

    criando tabelas com flyway

---
+++ backend/src/main/resources/V001__init.sql
@@ -0,0 +1,10 @@
+create table candidatura (salario decimal(38,2), status tinyint, id bigint not null auto_increment, plataforma_id bigint not null, tecnologia_id bigint not null, descricao varchar(255), empresa varchar(255), primary key (id)) engine=InnoDB;
+create table permissao (id bigint not null auto_increment, descricao varchar(255), nome varchar(255), primary key (id)) engine=InnoDB;
+create table plataforma (id bigint not null auto_increment, nome varchar(255), primary key (id)) engine=InnoDB;
+create table tecnologia (id bigint not null auto_increment, nome varchar(255), primary key (id)) engine=InnoDB;
+create table usuario (id bigint not null auto_increment, email varchar(255), nome varchar(255), senha varchar(255), primary key (id)) engine=InnoDB;
+create table usuario_permissao (permissao_id bigint not null, usuario_id bigint not null, primary key (permissao_id, usuario_id)) engine=InnoDB;
+alter table candidatura add constraint FK_candidatura_plataforma_id foreign key (plataforma_id) references plataforma (id);
+alter table candidatura add constraint FK_candidatura_tecnologia_id foreign key (tecnologia_id) references tecnologia (id);
+alter table usuario_permissao add constraint FK_usuario_permissao_permissao_id foreign key (permissao_id) references permissao (id);
+alter table usuario_permissao add constraint FK_usuario_permissao_usuario_id foreign key (usuario_id) references usuario (id);
