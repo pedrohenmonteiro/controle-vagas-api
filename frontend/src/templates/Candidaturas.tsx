ecb910eff67596b7e023a3e761bc6b9d8605dc2c
Pedro Monteiro <pedromont436@gmail.com>
2023-10-17 23:10:51 -0300

    refatorando componente Candidaturas  para aceitar uma lista de dados

--- frontend/src/templates/Candidaturas.tsx
+++ frontend/src/templates/Candidaturas.tsx
@@ -9,74 +9,65 @@ import Container from "../components/Container";
 import Text from "../components/Text";
 import { useEffect, useState } from "react";
 
-
 type CandidaturasProps = {
-  "id": number,
-  "empresa": string,
-  "descricao": string,
-  "status": string,
-  "salario": number,
-  "plataforma": {
-      "id": number,
-      "nome": string
-  },
-  "tecnologia": {
-      "id": number,
-      "nome": string
-  }
-}
-
-
+  id: number;
+  empresa: string;
+  descricao: string;
+  status: string;
+  salario: number;
+  plataforma: {
+    id: number;
+    nome: string;
+  };
+  tecnologia: {
+    id: number;
+    nome: string;
+  };
+};
 
 export default function Candidaturas() {
+  const [data, setData] = useState<CandidaturasProps[]>();
 
-  const [data, setData] = useState<CandidaturasProps>()
-  
   useEffect(() => {
-    fetch('http://localhost:8080/candidaturas/11')
-    .then(response => response.json())
-    .then(apiData => {
-      setData(apiData); 
-      console.log(apiData)
-    })
-  
-  }, [])
-  
+    fetch("http://localhost:8080/candidaturas")
+      .then((response) => response.json())
+      .then((apiData) => {
+        setData(apiData);
+        console.log(apiData);
+      });
+  }, []);
 
-  return data && (
-    <Container className="h-screen">
-      <div className="shadow-lg flex flex-col gap-8 p-6 m-12">
-      <Title icon={<BsBookmark />}>Minhas candidaturas</Title>
-      <Navigation />
-      
-      <div className="flex items-start justify-between">
-        <div className="flex flex-col gap-1">
-            <Title>{data?.empresa}</Title>
-            <Text medium>{data?.descricao}</Text>
-            <div className="flex gap-4">
-              <Text tecnologia={data?.tecnologia.nome}>{data?.tecnologia.nome}</Text>
-              <Text icon={<RiMoneyDollarCircleLine />}>{data?.salario}</Text>
-              <Text>{data?.plataforma.nome}</Text>
-            </div>
+  return (
+    data && (
+      <Container className="h-screen">
+        <div className="shadow-lg flex flex-col gap-8 p-6 m-12">
+          <Title icon={<BsBookmark />}>Minhas candidaturas</Title>
+          <Navigation />
+
+          {data.map((data) => {
+            return (
+              <div className="flex items-start justify-between">
+                <div className="flex flex-col gap-1">
+                  <Title>{data?.empresa}</Title>
+                  <Text medium>{data?.descricao}</Text>
+                  <div className="flex gap-4">
+                    <Text tecnologia={data?.tecnologia.nome}>
+                      {data?.tecnologia.nome}
+                    </Text>
+                    <Text icon={<RiMoneyDollarCircleLine />}>
+                      {data?.salario}
+                    </Text>
+                    <Text>{data?.plataforma.nome}</Text>
+                  </div>
+                </div>
+                <Button icon={<RxUpdate />} icon2={<SlArrowDown />}>
+                  Atualizar
+                </Button>
+              </div>
+            );
+          })}
         </div>
-              <Button icon={<RxUpdate />} icon2={<SlArrowDown />}>Atualizar</Button>
-      </div>
-
-      <div className="flex items-start justify-between">
-        <div className="flex flex-col gap-1">
-            <Title>Desenvolvedor Backend</Title>
-            <Text medium>Vaga de desenvolvimento backend</Text>
-            <div className="flex gap-4">
-              <Text tecnologia="Golang">Golang</Text>
-              <Text icon={<RiMoneyDollarCircleLine />}> 2.5k mês</Text>
-              <Text>LinkedIn</Text>
-            </div>
-        </div>
-              <Button icon={<RxUpdate />} icon2={<SlArrowDown />}>Atualizar</Button>
-      </div>
-
-      
-      </div>
-    </Container>
+      </Container>
+    )
   );
 }
