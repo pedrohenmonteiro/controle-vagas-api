48e1b256c4275e5b322b55f9326ea2a735eb40cf
Pedro Monteiro <pedromont436@gmail.com>
2023-10-17 23:07:15 -0300

    resolvendo problema de desempenho de n+1

--- backend/src/main/java/com/mont/controlevagas/domain/repository/CandidaturaRepository.java
+++ backend/src/main/java/com/mont/controlevagas/domain/repository/CandidaturaRepository.java
@@ -3,12 +3,17 @@ package com.mont.controlevagas.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mont.controlevagas.domain.model.Candidatura;
 import com.mont.controlevagas.domain.model.CandidaturaStatus;
 
 public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
  
+    @Query("from Candidatura c join fetch c.tecnologia join fetch c.plataforma")
+    List<Candidatura> findAll();
+
+    @Query("from Candidatura c join fetch c.tecnologia join fetch c.plataforma where c.status = :status" )
     List<Candidatura> findByStatus(CandidaturaStatus status);
 
 }
