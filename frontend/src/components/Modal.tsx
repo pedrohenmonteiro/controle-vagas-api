2b4cfb3a30ef23ac44e05dd58945f5f85585a4a9
Pedro Monteiro <pedromont436@gmail.com>
2023-10-19 13:40:05 -0300

    ajustando a posicao do botao de fechar mdoal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -11,20 +11,19 @@ export default function Modal({ isVisible, onClose, children }: ModalProps) {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center">
-      <div
-        className="bg-black opacity-25 fixed inset-0"
-        id="wrapper"
-        onClick={handleClose}
-      ></div>
-      <div className="w-[800px] relative">
+    <div
+      className="fixed inset-0 flex bg-black bg-opacity-25  blackdrop-blur-sm justify-center items-center"
+      id="wrapper"
+      onClick={handleClose}
+    >
+      <div className="w-[800px] flex flex-col ">
         <button
           className="text-white text-xl place-self-end"
           onClick={() => onClose()}
         >
           X
         </button>
-        <div className="bg-white p-2 rounded-lg relative z-10">{children}</div>
+        <div className="bg-white p-2 rounded-lg ">{children}</div>
       </div>
     </div>
   );
