a98be01feb21e70f3f2f3f951032b495ab8c9f3e
Pedro Monteiro <pedromont436@gmail.com>
2023-10-27 23:28:25 -0300

    implementando login POST

--- frontend/src/components/FormSignIn.tsx
+++ frontend/src/components/FormSignIn.tsx
@@ -2,19 +2,49 @@ import { useState } from "react";
 import TextField from "./TextField";
 import Button from "./Button";
 import Title from "./Title";
+import { useNavigate } from "react-router-dom";
 
 export default function FormSignIn() {
+  const navigate = useNavigate();
+
   const [loading, setLoading] = useState(false);
-  const [values, setValues] = useState({ email: "", senha: "" });
+  const [values, setValues] = useState({ username: "", password: "" });
+
+  const clientId = "myclientid";
+  const clientSecret = "myclientsecret";
 
   const handleInput = (field: string, value: string) => {
     setValues((s) => ({ ...s, [field]: value }));
   };
 
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setLoading(true);
     console.log(values);
+
+    try {
+      const response = await fetch("http://localhost:8080/oauth2/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+        },
+        body: `grant_type=password&username=${values.username}&password=${values.password}`,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        const token = data.access_token;
+
+        navigate("/candidaturas");
+
+        localStorage.setItem("access_token", token);
+      } else {
+        console.error("Erro de autenticação");
+      }
+    } catch (error) {
+      console.error("Erro. Tente novamente mais tarde.", error);
+    }
   };
 
   return (
@@ -22,15 +52,15 @@ export default function FormSignIn() {
       <div className="flex flex-col gap-5 ">
         <Title>Entrar</Title>
         <TextField
-          name="email"
+          name="username"
           label="Email"
-          onInputChange={(v) => handleInput("email", v)}
+          onInputChange={(v) => handleInput("username", v)}
         />
         <TextField
-          name="senha"
+          name="password"
           label="Senha"
           type="password"
-          onInputChange={(v) => handleInput("senha", v)}
+          onInputChange={(v) => handleInput("password", v)}
         />
 
         <Button color="blue" bold type="submit" disabled={loading}>
