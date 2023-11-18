83eaf2079a87fd2eaf9e15b3baa55b16cfa4a651
Pedro Monteiro <pedromont436@gmail.com>
2023-11-17 23:38:31 -0300

    alterando estilo ao clicar em um li em Navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 type NavigationProps = {
   onFilterChange: (v: "Aprovado" | "Reprovado" | "Em análise") => void;
 };
 
 export default function Navigation({ onFilterChange }: NavigationProps) {
-  const buttonStyle =
-    "hover:shadow-md transition-all duration-300 ease-in-out hover:bg-white hover:text-gray-700 rounded-lg py-2 px-8 cursor-pointer";
+  const [selectedFilter, setSelectedFilter] = useState<
+    "Aprovado" | "Reprovado" | "Em análise"
+  >("Aprovado");
+
+  const handleFilterChange = (
+    filter: "Aprovado" | "Reprovado" | "Em análise"
+  ) => {
+    setSelectedFilter(filter);
+    onFilterChange(filter);
+  };
+
+  const getButtonStyle = (filter: "Aprovado" | "Reprovado" | "Em análise") => {
+    return `hover:shadow-md transition-all duration-300 ease-in-out ${
+      selectedFilter === filter ? "bg-white shadow-md" : ""
+    } hover:bg-white hover:text-gray-700 rounded-lg py-2 px-8 cursor-pointer`;
+  };
 
   return (
-    <nav className="bg-gray-100 rounded py-1 px-1">
+    <nav className="bg-gray-100 rounded-lg py-1 px-1">
       <ul className="flex gap-1 justify-between text-lg font-semibold text-gray-500">
         <li
-          className={`bg-white shadow-md ${buttonStyle}`}
-          onClick={() => onFilterChange("Aprovado")}
+          className={getButtonStyle("Aprovado")}
+          onClick={() => handleFilterChange("Aprovado")}
         >
           Aprovado
         </li>
         <li
-          className={`${buttonStyle}`}
-          onClick={() => onFilterChange("Reprovado")}
+          className={getButtonStyle("Reprovado")}
+          onClick={() => handleFilterChange("Reprovado")}
         >
           Reprovado
         </li>
         <li
-          className={`${buttonStyle}`}
-          onClick={() => onFilterChange("Em análise")}
+          className={getButtonStyle("Em análise")}
+          onClick={() => handleFilterChange("Em análise")}
         >
           Em análise
         </li>
